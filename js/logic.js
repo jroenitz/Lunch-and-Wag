f09b1a9d04d2464047ac8d998343e1997143c0b6
Kyle Dahl <kyle.dahl201@gmail.com>
2020-03-31 10:46:09 -0500

    changed file structure, added jQuery file, ran working JS code.

---
+++ js/logic.js
@@ -0,0 +1,35 @@
+
+//preceeds all jquery code
+$(document).ready(function () {
+
+
+    //check if JS is loaded properly 
+    console.log("ready!")
+
+
+   
+
+
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+        
+
+});
